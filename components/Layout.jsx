efeb59e2cd1cfc62afa45febd903ed3ccb0a3527
saim770 <121598334+saim770@users.noreply.github.com>
2024-03-01 17:25:28 +0500

    Update Layout.jsx

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -8,7 +8,7 @@ const Layout = ({ children }) => {
   return (
     <div className="layout">
       <Head>
-        <title>My-Store</title>
+        <title>Shop</title>
       </Head>
       <header>
         <Navbar />
